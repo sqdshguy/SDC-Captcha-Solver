42627380376cfdf752ebc46fa7d47dabf9f00fed
Vlad Ciphersky <senioreighteen@yandex.ru>
2020-04-04 16:34:47 +0500

    • Changes [1/3]

--- sdc.js
+++ sdc.js
@@ -1,51 +1,38 @@
-// SDC Captcha Solver
-// by Mrsasha45op
-// 2020
+// jshint esversion: 6
+const config = {
+    solveKey: "ТОКЕН_OCR_SPACE",
+    token: "ТОКЕН_БОТА",
+    channel: {
+        id: "ID_КАНАЛА_ДЛЯ_ПРОПИСАНИЯ_КОМАНД",
+        message: "s.up"
+    }
+};
 
-const https = require('follow-redirects').https,
-      fs = require('fs');
+const solve = require("sdc-captcha-solver");
+const { Client } = require('discord.js');
 
-function solve(url, apikey){
-  const options = {
-    'method': 'POST',
-    'hostname': 'api.ocr.space',
-    'path': '/parse/image',
-    'headers': {
-      'apikey': apikey
-    },
-    'maxRedirects': 20
-  };
-  const urlToCaptcha = url;
-  return new Promise((resolve, reject) => {
-    const req = https.request(options, (res) => {
-      var chunks = [];
-    
-      res.on("data", (chunk) => {
-        chunks.push(chunk);
-      });
-    
-      res.on("end", (chunk) => {
-        const body = Buffer.concat(chunks);
-        resolve(JSON.parse(body).ParsedResults[0].ParsedText);
-      });
-    
-      res.on("error", (error) => {
-        reject(error);
-      });
-    });
-    
-    const postData = `------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"language\"\r\n\r\neng\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"isOverlayRequired\"\r\n\r\nfalse\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"url\"\r\n\r\n${urlToCaptcha}\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"iscreatesearchablepdf\"\r\n\r\nfalse\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"issearchablepdfhidetextlayer\"\r\n\r\nfalse\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--`;
-    req.setHeader('content-type', 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW');
-    req.write(postData);
-    req.end();
-  })
- 
+const client = new Client({
+    disableEveryone: true
+});
+
+function bump(chID, chCommand) {
+    console.info('WAIT | Попытка бампнуть сервер..');
+    return client.channels.get(chID).send(chCommand);
 }
 
-module.exports = solve;
+client.on("ready", () =>
+    bump(config.channel.id, config.channel.message)
+);
 
+client.on("message", (message) => {
+    if(message.author.id == "464272403766444044" && message.channel.id == config.channel.id) 
+        return solve(message.attachments.first().url, config.solveKey)
+            .then((response) => {
+                message.channel.send(response);
+                return client.setInterval(() => {
+                    bump(config.channel.id, config.channel.message);
+                }, 18000000);
+            });
+});
 
-
-
-
-
+client.login(config.token);
