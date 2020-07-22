e60c919081857872b2c17f660d62acc7b6f19522
Alexander Herasymov <drgerasimova8@gmail.com>
2020-07-22 12:36:58 +0300

    Delete some stuff

--- sdc.js
+++ sdc.js
@@ -1,6 +1,5 @@
 // SDC Captcha Solver
-// by Mrsasha45op
-// 2020
+//    by Mrsasha45op    2020
 
 const https = require('follow-redirects').https,
       fs = require('fs');
@@ -33,19 +32,12 @@ function solve(url, apikey){
         reject(error);
       });
     });
-    
     const postData = `------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"language\"\r\n\r\neng\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"isOverlayRequired\"\r\n\r\nfalse\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"url\"\r\n\r\n${urlToCaptcha}\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"iscreatesearchablepdf\"\r\n\r\nfalse\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"issearchablepdfhidetextlayer\"\r\n\r\nfalse\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--`;
     req.setHeader('content-type', 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW');
     req.write(postData);
     req.end();
   })
- 
+
 }
 
 module.exports = solve;
-
-
-
-
-
-
